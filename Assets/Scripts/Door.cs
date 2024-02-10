52e3534bc941353682d751de587cd6f953e7c6ac
LazyLuzon <[lazyluzon@gmail.com]>
2024-02-09 22:56:44 -0500

    Customizer Body Update
    Updated the body tilesheets to include the new actions. The new actions included the magic for fire, water, wind, and earth.
    
    Also started making the scriptable objects for the item database.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject exterior;
+    public GameObject player;
+    public float enterPositionX;
+    public float enterPositionY;
+    public float exitPositionX;
+    public float exitPositionY;
+
+    public void UseDoor()
+    {
+        if (exterior.activeInHierarchy == true)
+        {
+            exterior.SetActive(false);
+            player.transform.position = new Vector3(enterPositionX, enterPositionY,0);
+
+        } else
+        {
+            exterior.SetActive(true);
+            player.transform.position = new Vector3(exitPositionX, exitPositionY, 0);
+        }
+
+    }
+}
