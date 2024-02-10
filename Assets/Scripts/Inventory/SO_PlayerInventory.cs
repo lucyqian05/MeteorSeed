437ce4a72daf0b52b6b3f5e23e0bd6936bfaf666
LazyLuzon <[lazyluzon@gmail.com]>
2024-02-10 18:08:56 -0500

    Item Data Formatting
    Formatted the Item Data Scriptable Object to have item type specific drop down menus. I plan to build out the game to see if it compiles without any errors.

---
+++ Assets/Scripts/Inventory/SO_PlayerInventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player Inventory", menuName = "Player Inventory")]
+
+public class SO_PlayerInventory : ScriptableObject
+{
+
+    public List<SO_ItemData> items = new List<SO_ItemData>();
+
+}
