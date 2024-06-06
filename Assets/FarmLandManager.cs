fa0e7ba96676acfbf305284163fb0a50973c1558
Lazy Luzon <[lazyluzon@gmail.com]>
2024-06-06 08:01:54 -0400

    Farm Tile Connection to Magic
    The farm tiles change according to the magic used on it.

--- Assets/FarmLandManager.cs
+++ Assets/FarmLandManager.cs
@@ -1,18 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class FarmLandManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private RuleTile agniTile;
+
+    [SerializeField]
+    private Tile erdeTile;
+
+    [SerializeField]
+    private Tile biyoTile;
+
+    private Tilemap farmLand;
+    private Magic magic;
+    private PlayerInput playerInput;
+    private GameObject player; 
+
+    private void Start()
     {
-        
+        farmLand = GetComponent<Tilemap>();
+        playerInput = GetComponent<PlayerInput>();
+
+        player = GameObject.FindWithTag("Player");
+        magic = player.GetComponent<Magic>();
+
+
+        magic.OnAgni += SetLandAgni;
+        magic.OnErde += SetLandErde;
+        magic.OnBiyo += SetLandBiyo;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetLandAgni()
     {
-        
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tilePosition = farmLand.WorldToCell(worldPoint);
+        TileBase tile = farmLand.GetTile(tilePosition); 
+
+        if(tile != null)
+        {
+            farmLand.SetTile(tilePosition, agniTile);
+        }
+    }
+
+    private void SetLandErde()
+    {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tilePosition = farmLand.WorldToCell(worldPoint);
+        TileBase tile = farmLand.GetTile(tilePosition);
+
+        if (tile != null)
+        {
+            farmLand.SetTile(tilePosition, erdeTile);
+        }
+    }
+
+    private void SetLandBiyo()
+    {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tilePosition = farmLand.WorldToCell(worldPoint);
+        TileBase tile = farmLand.GetTile(tilePosition);
+
+        if (tile != null)
+        {
+            farmLand.SetTile(tilePosition, biyoTile);
+        }
     }
 }
